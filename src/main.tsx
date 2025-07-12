a2519d98c3936effd37a6108ca1150809c00088d
Alonso VN <alonsonh94@gmail.com>
2025-07-12 11:36:30 -0600

    fix: App component import

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 // import { Provider } from 'react-redux';
 // import { store } from './store/redux/store';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './app/App';
+import { App } from './app/App';
 
-import 'leaflet/dist/leaflet.css';
+// import 'leaflet/dist/leaflet.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
