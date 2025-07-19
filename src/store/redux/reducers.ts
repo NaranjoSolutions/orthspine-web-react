66c3d5ceddc0faee67909a55ac75c33695c84baf
Alonso VN <alonsonh94@gmail.com>
2025-07-18 20:42:41 -0600

    add user reducer

--- src/store/redux/reducers.ts
+++ src/store/redux/reducers.ts
@@ -1,9 +1,11 @@
 import { testimonialApi } from '../../api/clinicApi/testimonialApi';
 
 import testimonialsReducer from './slices/testimonialSlice';
+import userReducer from './slices/userSlice';
 
 export const reducers = {
   [testimonialApi.reducerPath]: testimonialApi.reducer,
 
   testimonial: testimonialsReducer,
+  user: userReducer,
 };
