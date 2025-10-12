3a641e9011eac8286e334a269428405176c12bd3
Alonso VN <alonsonh94@gmail.com>
2025-10-12 11:23:24 -0600

    feat: add auth store

--- src/store/redux/middlewares.ts
+++ src/store/redux/middlewares.ts
@@ -1,3 +1,5 @@
 import { testimonialApi } from '../../api/clinicApi/testimonialApi';
+import { authApi } from '@/features/auth/api/authApi';
 
-export const middlewares = (getDefaultMiddleware) => getDefaultMiddleware().concat(testimonialApi.middleware);
+export const middlewares = (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(testimonialApi.middleware, authApi.middleware);
--- src/store/redux/reducers.ts
+++ src/store/redux/reducers.ts
@@ -3,9 +3,15 @@ import { testimonialApi } from '../../api/clinicApi/testimonialApi';
 import testimonialsReducer from './slices/testimonialSlice';
 import userReducer from './slices/userSlice';
 
+import authReducer from '@/features/auth/store/authSlice';
+import { authApi } from '@/features/auth/api/authApi';
+
 export const reducers = {
   [testimonialApi.reducerPath]: testimonialApi.reducer,
 
   testimonial: testimonialsReducer,
   user: userReducer,
+
+  auth: authReducer,
+  [authApi.reducerPath]: authApi.reducer,
 };
