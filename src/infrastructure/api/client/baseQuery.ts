33e3432b742ce9e939d309b2dfd810ab2feca40c
Alonso VN <alonsonh94@gmail.com>
2025-11-21 19:56:21 -0600

    ref: set base url and api version values

--- src/infrastructure/api/client/baseQuery.ts
+++ src/infrastructure/api/client/baseQuery.ts
@@ -6,7 +6,7 @@ import { tokenService } from '@/features/auth/services/TokenService';
  * Automatically adds auth token to requests
  */
 export const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  baseUrl: import.meta.env.VITE_BASE_API_URL + import.meta.env.VITE_API_VERSION || 'http://localhost:8000/api/v1',
   prepareHeaders: (headers) => {
     // Get token from TokenService (Singleton)
     const token = tokenService.getAccessToken();
