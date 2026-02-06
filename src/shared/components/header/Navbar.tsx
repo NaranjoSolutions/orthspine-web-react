943405b9ed567ed696d36e65dc9cb16d8575aab1
Alonso VN <alonsonh94@gmail.com>
2026-02-05 23:08:55 -0600

    Update Navbar.tsx

--- src/shared/components/header/Navbar.tsx
+++ src/shared/components/header/Navbar.tsx
@@ -99,10 +99,8 @@ export const Navbar: React.FC = () => {
       <Drawer
         title={
           <div className={styles.drawerHeader}>
-            <div className={styles.logo}>
-              <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
-                <path d="M16 2L4 8v12c0 7.3 5.3 14.1 12 16 6.7-1.9 12-8.7 12-16V8L16 2z" fill="#007BB9" />
-              </svg>
+            <div className={styles.logo} onClick={() => navigate(ROUTE_PATHS.HOME)}>
+              <img src={logoSvg} alt="Orthopedic Spine Logo" className={styles.logoImage} />
               <span className={styles.logoText}>Orthopedic Spine</span>
             </div>
           </div>
