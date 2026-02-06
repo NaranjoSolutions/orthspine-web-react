f4158fbfb1811e4d43317b8b14a26e88421001d1
Alonso VN <alonsonh94@gmail.com>
2026-02-06 09:40:31 -0600

    update clinic address

--- src/shared/resources/clinic-information.ts
+++ src/shared/resources/clinic-information.ts
@@ -3,7 +3,7 @@ export const clinicInformation = {
   location: {
     latitude: 9.868032691676943,
     longitude: -83.90066555786967,
-    address: '100 mts Norte de la Municipalidad de San Rafael, Cartago',
+    address: 'De la Ferretería El Bosque, 50 m al este y 50 m al sur. San Rafael, Cartago, Costa Rica',
   },
   schedule: ['Lunes a Viernes: 8:00 am - 5:30 pm', 'Sábados: 8:30 am - 12:00 pm'],
   contact: {
